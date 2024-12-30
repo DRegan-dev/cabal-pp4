5f40d4bd58ad032acda4007bd4e854b799216efb
Damien Regan <damienreganmusic@gmail.com>
2024-12-30 11:03:39 +0000

    Add procfile for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cabal_sys.wsgi
--- cabal_sys/settings.py
+++ cabal_sys/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-dregandev-cabalpp4-s5n9vcrdd2a.ws.codeinstitute-ide.net', '.herokuapp.com' ]
 
