71bb9700173d3202c6606e464b8eee520a39b7cb
Damien Regan <damienreganmusic@gmail.com>
2024-12-30 16:14:52 +0000

    Create User model for database

--- cabal/models.py
+++ cabal/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#User roles for Promoters/Venues, Band/Artists, and Attendees
+class User(models.Model):
+    ROLE_CHOICES = [
+        ('promoter', 'Promoter/Venue'),
+        ('artist', 'Band/Artist'),
+        ('attendee', 'Attendee'),
+    ]
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    created_on = models.DateTimeField(auto_now_add=True)
