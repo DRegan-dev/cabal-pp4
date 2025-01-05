fcc141e40bf2969bc9d327eabb4629e95671c623
Damien Regan <damienreganmusic@gmail.com>
2025-01-05 17:08:13 +0000

    Update User model to extend AbstractUser

--- cabal/models.py
+++ cabal/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUserUser
 
 # Create your models here.
 
 #User roles for Promoters/Venues, Band/Artists, and Attendees
-class User(models.Model):
+class User(AbstactUser):
     ROLE_CHOICES = [
         ('promoter', 'Promoter/Venue'),
         ('artist', 'Band/Artist'),
@@ -14,6 +14,9 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
 
 # Venue/Promoter Model
 class Venue(models.Model):
@@ -28,7 +31,7 @@ class Artist(models.Model):
     name = models.CharField(max_length=255)
     genre = models.CharField(max_length=100)
     profile_description = models.TextField()
-    past_events = models.Manager('Event', related_name='Historical_artists')
+    past_events = models.ManyToManyField('Event', related_name='Historical_artists')
 
 # Ratings between Artists and Venues
 class VenueRating(models.Model):
@@ -43,7 +46,7 @@ class Event(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     date = models.DateTimeField()
-    venue = models.ForeignKey(Venue, on_delete=models.CASACADE, related_name='events')
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name='events')
     artists = models.ManyToManyField(Artist, related_name='events')
     attendees = models.ManyToManyField('Attendee', through='Ticket', related_name='events')
     ticket_price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -57,3 +60,12 @@ class EventStatistics(models.Model):
 class Attendee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     events_registered = models.ManyToManyField(Event, through='Ticket', related_name='registered_attendees')
+
+class Ticket(models.Model):
+    attendee = models.ForeignKey('Attendee', on_delete=models.CASCADE, related_name='tickets')
+    event = models.ForeignKey('Event', on_delete=models.CASCADE, related_name='tickets')
+    purchased_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Ticket for {self.event.title} - {self.attendee.user.username}"
+
