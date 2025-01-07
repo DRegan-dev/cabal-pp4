191d6265c70c39061cbedaba0b38e9a71290dd13
Damien Regan <damienreganmusic@gmail.com>
2025-01-07 19:43:03 +0000

    Update User model to AbstractUser

--- cabal/models.py
+++ cabal/models.py
@@ -1,17 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUserUser
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-#User roles for Promoters/Venues, Band/Artists, and Attendees
-class User(AbstactUser):
+#Custom User Model
+class User(AbstractUser):
     ROLE_CHOICES = [
         ('promoter', 'Promoter/Venue'),
         ('artist', 'Band/Artist'),
         ('attendee', 'Attendee'),
     ]
-    username = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     created_on = models.DateTimeField(auto_now_add=True)
 
