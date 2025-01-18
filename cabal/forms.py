5614c3037dbecf1276a4189fa8365dc4ddeb60d8
Damien Regan <damienreganmusic@gmail.com>
2025-01-18 15:50:01 +0000

    Add registration form to forms.py

---
+++ cabal/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class UserRegistrationForm(UserCreationform):
+    ROLE_CHOICES = [
+        ('attendee', 'Attendee'),
+        ('promoter', 'Promoter'),
+        ('Artist', 'Artist'),
+    ]
+
+    role = forms.ChoiceField(choices=ROLE_CHOICES)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'role', 'password1', 'password2']
--- cabal/urls.py
+++ cabal/urls.py
@@ -7,10 +7,14 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+
+    # Dashboard Urls
     path('venue-dashboard/', views.venue_dashboard, name='venue_dashboard'),
     path('attendee-dashboard/', views.attendee_dashboard, name='attendee_dashboard'),
     path('events-dashboard/', views.events_dashboard, name='events_dashboard'),
+    path('artist-dashboard/', views.artist_dashboard, name='artist_dashboard'),
+    
     path('create-event/', views.create_event, name='create_event'),
     path('events/', views.events_list, name='events_list'),
-    path('artist-dashboard/', views.artist_dashboard, name='artist_dashboard'),
+    
 ]
