dae3883bedb5a722034948a94280e6acf9ab19dd
Damien Regan <damienreganmusic@gmail.com>
2025-01-18 15:39:40 +0000

    Add login, register and logout urls.py

--- cabal/urls.py
+++ cabal/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Authentication URLs
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
     path('venue-dashboard/', views.venue_dashboard, name='venue_dashboard'),
     path('attendee-dashboard/', views.attendee_dashboard, name='attendee_dashboard'),
     path('events-dashboard/', views.events_dashboard, name='events_dashboard'),
